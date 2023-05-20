ed1c9cf48ad031ddf405839d7c4ec57437b855fb
rish15 <rish.cr7mini@gmail.com>
2023-05-21 02:23:41 +0530

    chore: added performance function to calculate latency

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
+const { performance } = require("perf_hooks");
+
 function APILogger(req, res, next) {
-  const startTime = process.hrtime();
+  const startTime = performance.now();
+
   const requestId = require("crypto").randomBytes(16).toString("hex");
   req.headers["X-request-id"] = requestId;
   res.on("finish", () => {
-    const elapsedTimeInMs = process.hrtime(startTime)[1] / 1000000; // Convert to milliseconds
+    const endTime = performance.now();
+    const latency = endTime - startTime;
+    console.log(`API latency: ${latency.toFixed(2)} ms`);
     const logData = {
       requestUrl: req.protocol + "://" + req.get("host") + req.originalUrl,
       method: req.method,
       userAgent: req.headers["user-agent"],
       xRequestId: req.headers["X-request-id"],
-      apiLatency: elapsedTimeInMs,
+      apiLatency: latency,
       apiCalledAt: new Date().toISOString(),
       reqBody: req.body,
       env: process.env.NODE_ENV,
