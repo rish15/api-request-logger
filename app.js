f3f09b145b125a59de83738a89d5aa5702c429c0
rish15 <rish.cr7mini@gmail.com>
2023-06-08 00:40:59 +0530

    chore: x-request-id -> xRequestId

--- app.js
+++ app.js
@@ -12,12 +12,14 @@ function APILogger(req, res, next) {
       requestUrl: req.protocol + "://" + req.get("host") + req.originalUrl,
       requestMethod: req.method,
       userAgent: req.headers["user-agent"],
-      'x-request-id': req.headers["x-spintly-id"],
+      xRequestId: req.headers["X-request-id"]
+        ? req.headers["X-request-id"]
+        : req.headers["x-spintly-id"],
       apiLatency: latency,
       time: new Date().toISOString(),
       reqBody: req.body,
       env: process.env.NODE_ENV,
-      msg: 'api_stats'
+      msg: "api_stats",
     };
     console.log(JSON.stringify(logData));
   });
